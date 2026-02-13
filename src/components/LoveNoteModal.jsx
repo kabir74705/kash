c3bcfbb31f20b942f58fa90cd72638e9cc69d286
kabir <kabirhansrajani22@gmail.com>
2026-02-13 23:37:48 +0530

    new

--- src/components/LoveNoteModal.jsx
+++ src/components/LoveNoteModal.jsx
@@ -12,7 +12,7 @@ export default function LoveNoteModal({ isOpen, onClose }) {
         <h2>To My Forever 💖</h2>
         <p className="">
 
-          Chari finally apna pehla bina ldai vala valentines day aa gya. Dhor thankuu tune mera ye week bhott bhott special bnane ke lie, tera chocolates bheja tera vo pyara sa teddy sb bhott pyara thaa, Mere lie lifetime memorable moments rhege sb . Gdhi me bhot lucky hu ki tu meri life partner h , Tu sath h to sb special h. 
+          Chari finally apna pehla bina ldai vala valentines day aa gya. Dhor thankuu mera ye week itna special bnane ke lie, tera chocolates bheja tera vo pyara sa teddy sb bhott pyara thaa, Mere lie lifetime memorable moments rhege  ye sb . Gdhi me bhot lucky hu ki tu meri life partner h , Tu sath h to sb special h. 
         </p>
 
       </div>
